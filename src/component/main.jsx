bff6c937e1c07bda9ea71755edd5bddb5cf5e346
maycolmolina <maycolmolinaarka12@gmail.com>
2024-07-30 10:03:51 -0600

    este camcio lo hice yo

--- src/component/main.jsx
+++ src/component/main.jsx
@@ -6,6 +6,8 @@ export default function Main() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>esta modificacion la hiso adrian</Text>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
